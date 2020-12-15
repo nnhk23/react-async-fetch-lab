1ae8c5f98fd35b9d88e9551df612b86ef536c604
Kim Nguyen <nnhk23@gmail.com>
2020-12-14 17:31:37 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React from 'react';
+
+export default class App extends React.Component{
+
+    state = {
+        people: []
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state.people.map(p => <h1 key={p.name}>{p.name}</h1>)}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(json => this.setState({people: json.people}))
+    }
+}
